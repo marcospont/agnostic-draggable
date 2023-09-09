4ba23e9f5577eb73109f078164dcb224ecc4369e
mpont <marcos.pont@gmail.com>
2023-09-09 18:43:47 -0300

    chore(deps): dependency upgrades

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -2,7 +2,7 @@ import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import babel from '@rollup/plugin-babel';
 import replace from '@rollup/plugin-replace';
-import { terser } from 'rollup-plugin-terser';
+import terser from '@rollup/plugin-terser';
 
 export default [
 	{
